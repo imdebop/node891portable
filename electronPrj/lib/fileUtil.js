8ae37923c2cad1ed11578bab8e6f1bedc24790a6
imdebop <daiho_hayashi@nifty.ocm>
2017-11-27 09:35:28 +0900

    init repository

---
+++ electronPrj/lib/fileUtil.js
@@ -0,0 +1,18 @@
+'use strict';
+
+var glob = require('glob');
+
+var fileUtil = {
+  fetchReadmeList: function (cb) {
+//    glob('../node_modules/**/README.md', function (err, matches) {
+    glob('in*', function (err, matches) {
+      if(err) {
+        cb(err, null);
+        return;
+      }
+      cb(null, matches);
+    });
+  }
+};
+
+module.exports = fileUtil;
